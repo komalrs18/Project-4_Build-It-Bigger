3e25387fba24e620803c17aac1937e43621628c6
komalrs18 <komalrs@yahoo.com>
2016-06-06 20:25:00 -0700

    Update Code

---
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -0,0 +1,93 @@
+package com.udacity.gradle.builditbigger;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    private InterstitialAd mInterstitialAd;
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        final View root = inflater.inflate(R.layout.fragment_main, container, false);
+
+        AdView mAdView = (AdView) root.findViewById(R.id.adView);
+        // Create an ad request. Check logcat output for the hashed device ID to
+        // get test ads on a physical device. e.g.
+        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        mAdView.loadAd(adRequest);
+
+
+        Button buttonX = (Button)root.findViewById(R.id.tell_button);
+// Register the onClick listener with the implementation above
+        buttonX.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                tellJoke(root);
+            }
+        });
+
+        return root;
+    }
+
+    public void tellJoke(View view){
+
+        ProgressBar progressBar = (ProgressBar) view.findViewById(R.id.progressbar);
+        final EndpointsAsyncTask task = new EndpointsAsyncTask(progressBar, getContext());
+
+        mInterstitialAd = new InterstitialAd(getContext());
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                super.onAdLoaded();
+
+                if (mInterstitialAd.isLoaded())
+                    mInterstitialAd.show();
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                super.onAdFailedToLoad(errorCode);
+
+                task.execute();
+            }
+
+            @Override
+            public void onAdClosed() {
+                task.execute();
+            }
+        });
+        AdRequest ar = new AdRequest
+                .Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+
+        try {
+            mInterstitialAd.loadAd(ar);
+        } catch(java.lang.NoClassDefFoundError error) {
+            error.printStackTrace();
+        }
+    }
+
+}
